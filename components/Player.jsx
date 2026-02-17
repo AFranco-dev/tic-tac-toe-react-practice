818b369a2f293a81793539dbd1bd87b4d37c5c77
Andre Franco <andre.franco.dev@gmail.com>
2026-02-17 16:05:46 -0600

    FEAT - added isEditing conditional logic on player component input

--- components/Player.jsx
+++ components/Player.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 
 function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((oldIsEditing) => !oldIsEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) playerName = <input type="text" value={name} required />;
+
   return (
     <li>
-      <span className="player-name">{name}</span>
-      <span className="player-symbol">{symbol}</span>
-      <button>Edit</button>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
